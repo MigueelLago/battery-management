d223b6624c8c308768ce5841617b8afa730a08f5
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-08-29 16:14:58 -0300

    refactor SystemInfoPort interface to improve method signatures for clarity

--- internal/core/ports/system_info.go
+++ internal/core/ports/system_info.go
@@ -3,8 +3,8 @@ package ports
 import "system-management/internal/core/models"
 
 type SystemInfoPort interface {
-	GetMemory(info models.MemoryInfo, err error)
-	GetDisk(info models.DiskInfo, err error)
-	GetNetwork(info models.NetworkInfo, err error)
-	GetBattery(info models.BatteryInfo, err error)
+	GetMemory() (models.MemoryInfo, error)
+	GetDisk(rootMount string) (models.DiskInfo, error)
+	GetNetwork() ([]models.NetworkInfo, error)
+	GetBattery() (models.BatteryInfo, error)
 }
