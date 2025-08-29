12709b4b13233936a6cb618eab7127dc87adae9b
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-08-29 16:14:42 -0300

    update SystemSnapshot struct to use a slice for NetworkInfo

--- internal/core/models/systemSnapshot.go
+++ internal/core/models/systemSnapshot.go
@@ -2,8 +2,8 @@ package models
 
 // Structure representing a snapshot of the system's state
 type SystemSnapshot struct {
-	Memory  MemoryInfo  `json:"memory"`
-	Disk    DiskInfo    `json:"disk"`
-	Network NetworkInfo `json:"network"`
-	Battery BatteryInfo `json:"battery"`
+	Memory  MemoryInfo    `json:"memory"`
+	Disk    DiskInfo      `json:"disk"`
+	Network []NetworkInfo `json:"network"`
+	Battery BatteryInfo   `json:"battery"`
 }
