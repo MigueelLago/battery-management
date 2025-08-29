a05c58e84d367bcbe38747c77a5c5e51ce69dad5
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-08-29 15:03:46 -0300

    add MemoryInfo struct for memory usage tracking

---
+++ internal/core/models/memory.go
@@ -0,0 +1,8 @@
+package models
+
+type MemoryInfo struct {
+	TotalBytes  uint64  `json:"total_bytes"`
+	UsedBytes   uint64  `json:"used_bytes"`
+	FreeBytes   uint64  `json:"free_bytes"`
+	UsedPercent float64 `json:"used_percent"`
+}
